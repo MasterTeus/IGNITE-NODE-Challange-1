2566391cb3878f780bb8a9ca70a2d9e073ba9cbc
Mateus Cazuza <39800209+MasterTeus@users.noreply.github.com>
2021-09-29 08:46:02 -0300

    feat: edite todo

--- src/index.js
+++ src/index.js
@@ -1,40 +1,82 @@
-const express = require('express');
-const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const express = require("express");
+const cors = require("cors");
+const { v4: uuid } = require("uuid");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  next();
 }
 
-app.post('/users', (request, response) => {
+app.post("/users", (request, response) => {
+  const { username, name } = request.body;
+
+  const body = {
+    id: uuid(),
+    username,
+    name,
+    todos: [],
+  };
+
+  users.push(body);
+
+  return response.status(201).json(body);
+});
+
+app.get("/todos", checksExistsUserAccount, (request, response) => {
+  const { username } = request.headers;
+
+  const user = users.filter((user) => user.username === username)[0];
+
+  return response.status(201).json(user.todos);
+});
+
+app.post("/todos", checksExistsUserAccount, (request, response) => {
+  // Complete aqui
+  const { username } = request.headers;
+  const { title, deadline } = request.body;
+
+  const todo = {
+    id: uuid(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date(),
+  };
+
+  const userTodos = users.filter((user) => user.username === username)[0];
+
+  userTodos.todos.push(todo);
+
+  return response.status(201).json(userTodos);
+});
+
+app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
+  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+  const { title, deadline } = request.body;
+
+  const userFiltred = users.find((user) => user.username === username);
+
+  const todo = userFiltred.todos.find((user) => user.id === id);
+
+  todo.title = title;
+  todo.deadline = deadline;
+
+  return response.status(201).json(todo);
+});
+
+app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
   // Complete aqui
 });
 
-app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
-
-app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
-
-app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
-
-app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
-
-app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
+app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
   // Complete aqui
 });
 
